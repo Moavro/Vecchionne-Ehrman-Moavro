510822fa47025df955f43644bf62df358a876666
NicoVecchione <nvecchione@udesa.edu.ar>
2023-04-11 17:56:03 -0300

    Carpeta data, usuario, comentarios, productos

---
+++ TFI/db/data.js
@@ -0,0 +1,98 @@
+const data = {
+    usuario : {
+        email: "tufobarra12@gmail.com",
+        usuario: "tufi12",
+        contraseña:"1234",
+        fechaNacimiento:"01-01-200",
+        documento:"45487691",
+        fotoPerfil:"/images/user/perfil.jfif"
+   },
+    productos: [
+        {
+            id: 1,
+            imagen: "/images/products/timbos1.webp",
+            nombreProducto: "Nike Phantom",
+            descripcion:"Botines que te ayudan a encontrar el toque perfecto",
+            fechaCarga:""
+        },{
+            id: 2,
+            imagen: "/images/products/timbos2.webp",
+            nombreProducto: "Nike Vapor",
+            descripcion:"Domina el balon con estos botines Adidas de corte bajo",
+            fechaCarga:""
+        },{
+            id: 3,
+            imagen: "/images/products/timbos3.webp",
+            nombreProducto: "Adidas Predator Edge",
+            descripcion:"Domina el balon con estos botines Adidas sin cordones",
+            fechaCarga:""
+        },{
+            id: 4,
+            imagen: "/images/products/timbos4.webp",
+            nombreProducto: "Adidas Goleto VIII",
+            descripcion:"Botines para pasto sintetico hechos parcialmente con contenido reciclado",
+            fechaCarga:""
+        },{
+            id: 5,
+            imagen: "/images/products/timbos5.webp",
+            nombreProducto: "Adidas Speedportal",
+            descripcion:"Botines para terreno blando que te ayudan a controlar cada parte de tu juego",
+            fechaCarga:""
+        },{
+            id: 6,
+            imagen: "/images/products/timbos6.webp",
+            nombreProducto: "Adidas Nemeziz",
+            descripcion:"Conntrola cada jugada con estos botines Adidas",
+            fechaCarga:""
+        },{
+            id: 7,
+            imagen: "/images/products/timbos7.webp",
+            nombreProducto: "New Balance Furon",
+            descripcion:"Logra tu mejor jugada conn estos bbotines hechos en parte con materiales reciclados",
+            fechaCarga:""
+        },{
+            id: 8,
+            imagen: "/images/products/timbos8.webp",
+            nombreProducto: "New Balance Tekela",
+            descripcion:"Botines veloces inspirados en Messi hechos parcialmente con materiales reciclados",
+            fechaCarga:""
+        },{
+            id: 9,
+            imagen: "/images/products/timbos9.webp",
+            nombreProducto: "New Balance Furon V7",
+            descripcion:"Domina los partidos sobre terreno firme con estos botines lleno de colores",
+            fechaCarga:""
+        },{
+            id: 10,
+            imagen: "/images/products/timbos10.webp",
+            nombreProducto: "Adidas Copa Pure",
+            descripcion:"Mejora tu juego con los suaves botines sinteticos de Adidas x Classic Lego",
+            fechaCarga:""
+        }     
+    ],
+    comentarios: [
+        {
+            nombreUsuario:"Chapu_Martinez",
+            texto:"Tremendo botin, lo recomiendo!",
+            fotoPerfil:"/images/user/perfil.jfif"
+        },{
+            nombreUsuario:"Doman",
+            texto:"Malisimo el botin! Muy mala calidad!",
+            fotoPerfil:"/images/user/perfil.jfif"
+        },{
+            nombreUsuario:"Fabra",
+            texto:"Con esos el diego se gambeteo a los ingleses",
+            fotoPerfil:"/images/user/perfil.jfif"
+        },{
+            nombreUsuario:"El_Zorro",
+            texto:"Ayer le clave 3 a River con esos",
+            fotoPerfil:"/images/user/perfil.jfif"
+        },{
+            nombreUsuario:"Demicheles",
+            texto:"Un fuegooo",
+            fotoPerfil:"/images/user/perfil.jfif"
+        }
+    ]
+}
+
+module.exports = data
