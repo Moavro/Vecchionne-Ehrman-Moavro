c25f69b417cff6e964915793f57171d0874c1288
NicoVecchione <nvecchione@udesa.edu.ar>
2023-04-13 18:21:35 -0300

    Arreglo header y rutas

--- TFI/routes/product.js
+++ TFI/routes/product.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 const productController= require('../controllers/productController')
 /* GET home page. */
-router.get('/:id', productController.show);
-router.get("product-add", productController.productAdd);
+router.get('/detail/:id', productController.show);
+router.get("/product-add", productController.productAdd);
 
 
 module.exports = router
