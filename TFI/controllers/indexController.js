7370a8a90cc4c98913faeeb5e59da26cbae0d6e1
NicoVecchione <nvecchione@udesa.edu.ar>
2023-06-02 12:17:09 -0300

    Avance Punto 8 (pagina home), detalle producto, Punto 5 (search results)

--- TFI/controllers/indexController.js
+++ TFI/controllers/indexController.js
@@ -1,14 +1,22 @@
-const data= require('../db/data')
 const db = require("../database/models");
+const op = db.Sequelize.Op;
 
 const indexController = {
     index: function(req,res){
-        return res.render('index', {
+        db.Producto.findAll({
+            order: [["fecha_carga", "DESC"]]    
+        }) .then((result) => {
+            return res.render ("index",{productos: result})
+        }).catch((err) => {
+            console.log(err)
+        });
+    },  
+/*         return res.render('index', {
             usuarios: data.usuario,
             productos: data.productos,
             comentados: [data.productos[0], data.productos[1], data.productos[2] ]
         })
-    },
+    }, */
 
     headerLogueado: function(req,res){
         return res.render("headerLogueado", {
@@ -17,10 +25,17 @@ const indexController = {
     },
 
     searchResult: function(req,res){
-        return res.render('search-result', {
-            usuarios: data.usuario,
-            productos: data.productos
-        })
+        let queryString = req.query.search
+
+        db.Producto.findAll({
+            where:[{title: {[op.like]:`%${queryString}$%`}}], order: [["fecha_carga", "DESC"]]
+        }) 
+        .then((result) => {
+            return res.render('search-result', {productos: result })
+        }).catch((err) => {
+            
+        });
+
     }
 }
 
